8cd50adf0bfa2aa1a18f17703de7026059c38f28
MiloszSzatkowski <milosz.dawid.szatkowski@gmail.com>
2019-03-15 12:43:43 +0000

    init

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+
+<meta name="viewport" content="width=device-width" />
+<?php wp_head(); ?>
+</head>
+
+<header></header>
+
+<div id="container">
